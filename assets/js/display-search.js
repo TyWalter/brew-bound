4eb5923d114d29a408ed75defafdfb907fac042b
TyWalter <walterty89@gmail.com>
2024-04-12 19:07:54 -0500

    Working on randomizer

---
+++ assets/js/display-search.js
@@ -0,0 +1,73 @@
+const formSel = document.querySelector('#form-selection');
+const resultTextEl = document.querySelector('#result-text');
+
+
+function getParams(){
+  const nameEl = localStorage.getItem('name');
+  const cityEl = localStorage.getItem('city');
+  const postalEl = localStorage.getItem('postal');
+  const typeEl = localStorage.getItem('type');
+
+  resultTextEl.textContent = `${cityEl}`;
+
+  searchApi(nameEl, cityEl, postalEl, typeEl);
+}
+
+
+function searchApi(nameSearch, citySearch, postalSearch, typeSearch){
+  const requestUrl = `https://api.openbrewerydb.org/v1/breweries?by_city=${citySearch}&by_name=${nameSearch}&by_postal=${postalSearch}&by_type=${typeSearch}&sort=type,name:${sortSearch}&per_page=${perPageSearch}`;
+  fetch(requestUrl)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log(error);
+    alert('You done messed up, A-A-ron');
+  })
+};
+
+
+function printResults(data){
+  for (let i=0; i<data.length; i++){
+    const article = data[i]
+    const titleEl = document.createElement('h3')
+    titleEl.textContent = article.title
+    const descEl = document.createElement('p')
+    descEl.textContent = article.description
+    const urlEl = document.createElement('a')
+    urlEl.textContent = 'Read More'
+    urlEl.setAttribute('href', article.url)
+    const card = document.createElement('div')
+    card.appendChild(titleEl)
+    card.appendChild(descEl)
+    card.appendChild(urlEl)
+    resultContentEl.appendChild(card)
+  }
+}
+
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const nameSearch = document.querySelector('#xxxxx').value;
+  const citySearch = document.querySelector('#xxxxx').value;
+  const postalSearch = document.querySelector('#xxxxx').value;
+  const typeSearch = document.querySelector('#xxxxx').value;
+
+  if (!citySearch) {
+    console.error('You need to enter a city!');
+    return;
+  } document.location.assign('search-results.html');
+    localStorage.setItem('Name', nameSearch);
+    localStorage.setItem('City', citySearch);
+    localStorage.setItem('Postal', postalSearch);
+    localStorage.setItem('Type', typeSearch);
+  }
+
+
+formSel.addEventListener('submit', formSubmit);
+
+getParams();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,58 @@
-const formEl = document.querySelector('#xxxxx');
-const nameSearch = document.querySelector('#xxxxx').value;
-const citySearch = document.querySelector('#xxxxx').value;
-const postalSearch = document.querySelector('#xxxxx').value;
-const typeSearch = document.querySelector('#xxxxx').value;
+const formSel = document.querySelector('#form-selection');
+const bestBrew = document.querySelector('#best-brew');
+
+
+function randomApi(){
+  const requestUrl = 'https://api.openbrewerydb.org/v1/breweries?per_page=200';
+  fetch(requestUrl)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+    printRandom(data)
+  })
+  .catch(function(error){
+    console.log(error);
+    alert('You done messed up, A-A-ron');
+  })
+};
+
+
+function printRandom(data){
+  for (let i=0; i<data.length; i++){
+    const article = data[Math.floor(Math.random()*data.length)]
+    console.log(article)
+    const nameEl = document.createElement('h3')
+    nameEl.textContent = article.name
+    const cityEl = document.createElement('p')
+    cityEl.textContent = article.city
+    const stateEl = document.createElement('p')
+    stateEl.textContent = article.state
+    const phoneEl = document.createElement('p')
+    phoneEl.textContent = article.phone
+    const urlEl = document.createElement('a')
+    urlEl.textContent = 'Visit Their Site'
+    urlEl.setAttribute('href', article.website_url)
+    const card = document.createElement('div')
+    card.appendChild(nameEl)
+    card.appendChild(cityEl)
+    card.appendChild(stateEl)
+    card.appendChild(phoneEl)
+    card.appendChild(urlEl)
+    bestBrew.appendChild(card)
+  }
+}
+
 
 
 function formSubmit(event) {
   event.preventDefault();
+
+  const nameSearch = document.querySelector('#xxxxx').value;
+  const citySearch = document.querySelector('#xxxxx').value;
+  const postalSearch = document.querySelector('#xxxxx').value;
+  const typeSearch = document.querySelector('#xxxxx').value;
 
   if (!citySearch) {
     console.error('You need to enter a city!');
@@ -19,21 +65,7 @@ function formSubmit(event) {
   }
 
 
-function searchApi(){
-  const requestUrl = `https://api.openbrewerydb.org/v1/breweries?by_city=${citySearch}&by_name=${nameSearch}&by_postal=${postalSearch}&by_type=${typeSearch}&sort=type,name:${sortSearch}&per_page=${perPageSearch}`;
-  fetch(requestUrl)
-  .then(function (response){
-    return response.json();
-  })
-  .then(function(data){
-    console.log(data);
-  })
-  .catch(function(error){
-    console.log(error);
-    alert('You done messed up, A-A-ron');
-  })
-};
+randomApi();
 
-searchApi();
 
-formEl.addEventListener('submit', formSubmit);
+formSel.addEventListener('submit', formSubmit);
